ec7de93f557cc5f3ccdc59fb3fbb0bdbf653135e
qianmengyu <2269368718@qq.com>
2023-10-18 19:10:25 +0800

    2-39

---
+++ Exercise-02/2-39.cpp
@@ -0,0 +1,17 @@
+/*
+ * 2-39
+ * 
+ * */
+
+#include<iostream>
+
+using namespace std;
+
+struct Foo {
+    /* 空的 */
+}; // expected ';' after struct definition 
+
+int main() {
+    
+    return 0;
+}
